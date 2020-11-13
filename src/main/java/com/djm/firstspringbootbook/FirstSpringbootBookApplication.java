b32bf000fcc57c6254c2f2e05af9b257a5b1cc56
dengjianming <2654827818@qq.com>
2020-11-13 16:08:40 +0800

    初始化提交,2020-11-13,第一次提交

---
+++ src/main/java/com/djm/firstspringbootbook/FirstSpringbootBookApplication.java
@@ -0,0 +1,13 @@
+package com.djm.firstspringbootbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringbootBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringbootBookApplication.class, args);
+    }
+
+}
